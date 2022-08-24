921b8d606caee2889d85b39a15d1ded6e136609f
mageche <mageche10@gmail.com>
2022-08-24 13:44:52 +0200

    Added basic Ores and Raw materials

--- src/main/java/mageche/massivemachinery/registres/ModBlocks.java
+++ src/main/java/mageche/massivemachinery/registres/ModBlocks.java
@@ -1,11 +1,13 @@
 package mageche.massivemachinery.registres;
 
 import mageche.massivemachinery.MassiveMachinery;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -20,13 +22,27 @@ public class ModBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MassiveMachinery.MOD_ID);
 
-    public static final RegistryObject<Block> BALSAM_FIR_PLANKS = registerBlock("balsam_fir_planks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)), ModCreativeTab.MASSIVEMACHINERY_TAB);
-    public static final RegistryObject<Block> BALSAM_FIR_LOG = registerBlock("balsam_fir_log", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)), ModCreativeTab.MASSIVEMACHINERY_TAB);
-    public static final RegistryObject<Block> BALSAM_FIR_WOOD = registerBlock("balsam_fir_wood", () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)), ModCreativeTab.MASSIVEMACHINERY_TAB);
+    public static final RegistryObject<Block> BALSAM_FIR_PLANKS = registerBlock("balsam_fir_planks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
+    public static final RegistryObject<Block> BALSAM_FIR_LOG = registerBlock("balsam_fir_log", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)));
+    public static final RegistryObject<Block> BALSAM_FIR_WOOD = registerBlock("balsam_fir_wood", () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)));
 
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+    public static final RegistryObject<Block> COPPER_ORE = registerBlock("copper_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.COPPER_ORE)));
+    public static final RegistryObject<Block> DEEPSLATE_COPPER_ORE = registerBlock("deepslate_copper_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_COPPER_ORE)));
+    public static final RegistryObject<Block> LEAD_ORE = registerBlock("lead_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+    public static final RegistryObject<Block> DEEPSLATE_LEAD_ORE = registerBlock("deepslate_lead_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE)));
+    public static final RegistryObject<Block> TIN_ORE = registerBlock("tin_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+    public static final RegistryObject<Block> DEEPSLATE_TIN_ORE = registerBlock("deepslate_tin_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE)));
+    public static final RegistryObject<Block> ZINC_ORE = registerBlock("zinc_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+    public static final RegistryObject<Block> DEEPSLATE_ZINC_ORE = registerBlock("deepslate_zinc_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE)));
+    public static final RegistryObject<Block> SULFUR_ORE = registerBlock("sulfur_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_ORE).strength(2.5F, 2.5F), UniformInt.of(1, 3)));
+    public static final RegistryObject<Block> DEEPSLATE_SULFUR_ORE = registerBlock("deepslate_sulfur_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE).strength(4F, 2.5F), UniformInt.of(1, 3)));
+    public static final RegistryObject<Block> BAUXITE_ORE = registerBlock("bauxite_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE).strength(4F, 4F)));
+    public static final RegistryObject<Block> DEEPSLATE_BAUXITE_ORE = registerBlock("deepslate_bauxite_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE).strength(5.5F, 3.5F)));
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        ModItems.ITEMS.register(name, () -> new BlockItem(toReturn.get(), new Item.Properties().tab(tab)));
+        ModItems.ITEMS.register(name, () -> new BlockItem(toReturn.get(), new Item.Properties().tab(ModCreativeTab.MASSIVEMACHINERY_TAB)));
         return toReturn;
     }
 
--- src/main/java/mageche/massivemachinery/registres/ModItems.java
+++ src/main/java/mageche/massivemachinery/registres/ModItems.java
@@ -1,6 +1,7 @@
 package mageche.massivemachinery.registres;
 
 import mageche.massivemachinery.MassiveMachinery;
+import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -14,6 +15,13 @@ import java.util.function.Supplier;
 public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MassiveMachinery.MOD_ID);
+
+    public static final RegistryObject<Item> RAW_BAUXITE = ITEMS.register("raw_bauxite", () -> new Item(new Item.Properties().tab(ModCreativeTab.MASSIVEMACHINERY_TAB)));
+    public static final RegistryObject<Item> RAW_COPPER = ITEMS.register("raw_copper", () -> new Item(new Item.Properties().tab(ModCreativeTab.MASSIVEMACHINERY_TAB)));
+    public static final RegistryObject<Item> RAW_LEAD = ITEMS.register("raw_lead", () -> new Item(new Item.Properties().tab(ModCreativeTab.MASSIVEMACHINERY_TAB)));
+    public static final RegistryObject<Item> RAW_TIN = ITEMS.register("raw_tin", () -> new Item(new Item.Properties().tab(ModCreativeTab.MASSIVEMACHINERY_TAB)));
+    public static final RegistryObject<Item> SULFUR = ITEMS.register("sulfur", () -> new Item(new Item.Properties().tab(ModCreativeTab.MASSIVEMACHINERY_TAB)));
+    public static final RegistryObject<Item> RAW_ZINC = ITEMS.register("raw_zinc", () -> new Item(new Item.Properties().tab(ModCreativeTab.MASSIVEMACHINERY_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
